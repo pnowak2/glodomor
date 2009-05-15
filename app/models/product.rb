8d47c53a45b821b9988d227f62d597ead7452d95
Piotr Nowak <p.nowak2@gmail.com>
2009-05-15 21:19:40 +0200

    zdjecie dla produktow

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :product_category
   has_many :line_items
+  has_attached_file :photo, :styles => { :medium => "320x200>", :thumb => "64x48>" }
+    
   named_scope :published, :conditions => { :published => true }
   named_scope :recent,
               lambda {|*args| {:conditions => ["created_at > ?", (args.first || 2.days.ago)], :limit => 3 } }
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090515181139) do
+ActiveRecord::Schema.define(:version => 20090515191507) do
 
   create_table "line_items", :force => true do |t|
     t.integer  "order_id"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(:version => 20090515181139) do
     t.datetime "updated_at"
     t.integer  "product_category_id"
     t.boolean  "published",           :default => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", :force => true do |t|
